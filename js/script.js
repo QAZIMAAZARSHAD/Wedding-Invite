2162ea70f6eb73c3f13d1702df4eda076b05eeec
Qazi Maaz Arshad <56196640+QAZIMAAZARSHAD@users.noreply.github.com>
2022-09-05 11:38:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
       $('.sakura-falling').sakura();
 })(jQuery);
 
-$(window).on( "load", function(){
+$(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi Mein Zaroor Aana');
 });
